81a74752be912582156d91295a91bfca0cb150d0
Burhanuddin <b.plumber@somaiya.edu>
2022-06-14 11:06:53 +0530

    705

---
+++ Day7/DesignHashSet.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+class MyHashSet:
+
+    def __init__(self):
+        self.dick= defaultdict(bool)
+
+    def add(self, key: int) -> None:
+        self.dick[key] = True
+
+    def remove(self, key: int) -> None:
+        self.dick[key] = False
+        
+    def contains(self, key: int) -> bool:
+        return self.dick[key]
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
