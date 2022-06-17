70abd830bb9c726aafa6a064062eba351f62690a
Burhanuddin <b.plumber@somaiya.edu>
2022-06-17 06:34:27 +0530

    771

---
+++ Day10/JewelsandStones.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        dick = dict(Counter(stones))
+        st = set(dick.keys()).intersection(set(jewels))
+        ct = 0
+        for i in st:
+            ct+=dick[i]
+        return ct
