b0b06dbee8ad78f3cc37c619da565041eaf7c004
Burhanuddin <b.plumber@somaiya.edu>
2022-06-18 06:43:10 +0530

    812

---
+++ Day11/LargestTriangleArea.py
@@ -0,0 +1,15 @@
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        max_a = 0.0
+        a = 0.0
+        for i in range(len(points)-2):
+            x1,y1=points[i]
+            for j in range(i+1,len(points)-1):
+                x2,y2=points[j]
+                for k in range(j+1,len(points)):
+                    x3,y3=points[k]
+                    a = abs(0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))) 
+                    if a > max_a :
+                        max_a = a
+        return max_a
+        
