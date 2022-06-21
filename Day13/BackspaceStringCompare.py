3ef9f9cd18011ee2d1a11270d0a560419409669a
Burhanuddin <b.plumber@somaiya.edu>
2022-06-21 07:29:16 +0530

    844

---
+++ Day13/BackspaceStringCompare.py
@@ -0,0 +1,21 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stackOne, stackTwo = [], []
+        
+        for i in s:
+            if i == '#':
+                if stackOne:
+                    stackOne.pop()
+                    
+            else:
+                stackOne.append(i)
+                
+        for i in t:
+            if i == '#':
+                if stackTwo:
+                    stackTwo.pop()
+            else:
+                stackTwo.append(i)
+                
+                
+        return stackOne == stackTwo
