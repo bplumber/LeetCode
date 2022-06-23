f94389cac1ad02c476255bc90e3516e03d14c469
Burhanuddin <b.plumber@somaiya.edu>
2022-06-23 06:07:20 +0530

    860

---
+++ Day15/Lemonade Change.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = 0
+        ten = 0
+        for i in bills:
+            if i == 5:
+                five+=1
+            elif i==10:
+                ten+=1
+                five-=1
+                if five<0:
+                    return False
+                    break
+            elif i == 20:
+                if ten>0:
+                    ten-=1
+                    five-=1
+                else:
+                    five-=3
+                if ten<0 or five<0:
+                    return False
+                    break
+        return True
