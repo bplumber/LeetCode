6758d8636ee06ab02018fd18b4894e78bf5ed5b5
Burhanuddin <b.plumber@somaiya.edu>
2022-06-25 06:42:14 +0530

    883

--- Day17/Projection Area of 3D Shapes.py
+++ Day17/Projection Area of 3D Shapes.py
@@ -1,20 +1,16 @@
 class Solution:
     def projectionArea(self, grid: List[List[int]]) -> int:
         x = 0
-        for i in grid:
-            for j in i:
-                if j!=0:
-                    x+=1
+        z = 0
         y = []
         for i in range(len(grid)):
             y.append(0)
         for i in range(len(grid)):
+            z+=max(grid[i])
             for j in range(len(grid[i])):
+                if grid[i][j]!=0:
+                    x+=1
                 if grid[i][j]>y[j]:
                     y[j] = grid[i][j]
-        y_sum = sum(y)
-        z = 0
-        for i in grid:
-            z+=max(i)
-        return x+y_sum+z
+        return x+sum(y)+z
         
