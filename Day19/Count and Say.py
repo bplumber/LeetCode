982bf186b519eecf7e32b3ac667604b9e48d4a18
Burhanuddin <b.plumber@somaiya.edu>
2022-06-28 14:42:25 +0530

    38

---
+++ Day19/Count and Say.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        x = 0
+        while x < n:
+            x+=1
+            if x == 1:
+                s = '1'
+            else:
+                tp = ''
+                i = 0
+                j = i+1
+                while j!=len(s):
+                    if s[i]!=s[j]:
+                        tp+=str(j-i)
+                        tp+=str(s[i])
+                        i = j
+                    j+=1
+                tp+=str(j-i)
+                tp+=(str(s[i]))
+                s = tp
+        return s
