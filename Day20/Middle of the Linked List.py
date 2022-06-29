d040ca2bc984cb27a1f4dc73fa835611fca2c6c0
Burhanuddin <b.plumber@somaiya.edu>
2022-06-29 11:43:06 +0530

    876

--- Day20/Middle of the Linked List.py
+++ Day20/Middle of the Linked List.py
@@ -4,7 +4,7 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def middleNode(self, head: ListNode)-> ListNode:
+    def middleNode(self, head: ListNode)-> ListNode :
         temp = head
         ct = 0 
         while temp:
